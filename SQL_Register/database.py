222a113275864e70102a4a8b82ca62a538b7dc23
diegoreiss <99758941+diegoreiss@users.noreply.github.com>
2022-06-18 21:58:04 -0300

    mercado

--- SQL_Register/database.py
+++ SQL_Register/database.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def Database(): 
     global conn, cursor
-    conn = sqlite3.connect("cadastro.db") # Conexão com o banco de dados
+    conn = sqlite3.connect("SQL_Register/cadastro.db") # Conexão com o banco de dados
     with conn:
         cursor = conn.cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS `usuarios` (id_usuarios INTEGER NOT NULL PRIMARY KEY  AUTOINCREMENT, nomeUsuario TEXT, senha TEXT)") # Cria uma tabela
